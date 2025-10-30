d494fc0c3662cba9f3b1ee3f8d8f741cf7273927
Bella Solomon-Falkfrii - CDA24 GBG <bella.solomon-falkfrii@iths.se>
2025-10-30 18:06:49 +0100

    responsive navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styling/header.css";
 import { Link } from "react-router-dom";
 import logo from "../images/logos/prel_logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
-    <>
-      <div id="header">
-        <div id="header-logo">
-          <Link to="/">
-            <img src={logo} id="header-logo" alt="RozWon logo" />
-          </Link>
-        </div>
-        <div className="navbar">
-          <Link to="/"> Home</Link>
-          <Link to="/aboutRozWon">About us</Link>
-          <Link to="/contactUs">Connect with us </Link>
-        </div>
+    <header id="header">
+      <div>
+        <Link to="/" onClick={closeMenu}>
+          <img id="header-logo" src={logo} alt="RozWon logo" />
+        </Link>
       </div>
-    </>
+
+      <div className="hamburger" onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <nav className={`navbar ${menuOpen ? "active" : ""}`}>
+        <Link to="/" onClick={closeMenu} className="navbar-link">
+          Home
+        </Link>
+        <Link to="/aboutRozWon" onClick={closeMenu} className="navbar-link">
+          About us
+        </Link>
+        <Link to="/contactUs" onClick={closeMenu} className="navbar-link">
+          Connect with us
+        </Link>
+      </nav>
+    </header>
   );
 };
 
